bdef4047f4babecd8071e77fdfbc7120b86cdc5a
Mezzo <mezzo9411@gmail.com>
2021-10-14 22:05:19 +0300

    feature: add FileUploadTest

---
+++ src/test/java/FileUploadTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FileUploadTest extends BaseTest{
+
+    @Test
+    public void FileUploadedSuccessfully(){
+        driver.get("http://the-internet.herokuapp.com/upload");
+        driver.findElement(By.id("file-upload")).sendKeys("C:/Users/Mezzo/Downloads/1920x1200_1633616315162.jpeg");
+        driver.findElement(By.cssSelector("[value=Upload]")).click();
+        Assert.assertEquals(driver.findElement(By.id("uploaded-files")).getText(),"1920x1200_1633616315162.jpeg");
+    }
+
+}
